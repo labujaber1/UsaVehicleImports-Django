b53f8f804148a48e059e121c9f3a5d6e378a4543
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2024-03-18 09:36:03 +0000

    fixed connection string

--- deployment.py
+++ deployment.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='): pair.split('=')[1] for pair in connection_string.split(' ')}
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
     'default': {
