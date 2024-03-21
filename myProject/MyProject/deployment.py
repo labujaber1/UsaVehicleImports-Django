f82022f1d9bbc014000d510fde525eaa8a5f67e3
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2024-03-21 11:44:22 +0000

    mm

--- myProject/MyProject/deployment.py
+++ myProject/MyProject/deployment.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CRSF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = False
 
 
