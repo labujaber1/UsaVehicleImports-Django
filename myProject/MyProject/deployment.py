eebfcea785c6d2cec916cde44a34361110cd0faa
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2024-03-21 10:56:02 +0000

    deploy file amend collectstatic

--- myProject/MyProject/deployment.py
+++ myProject/MyProject/deployment.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CRSF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
 
 
