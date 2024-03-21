7760e3a4ad7adc859f91d9bd3516669e9fb52812
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2024-03-21 13:06:22 +0000

    grr

--- myProject/MyProject/deployment.py
+++ myProject/MyProject/deployment.py
@@ -13,7 +13,7 @@ TEMPLATE_DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
